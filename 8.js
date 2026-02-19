a240fb3dc86e5dd3ef13ee4624517c7839fda098
Harshit Jain <harshitostwal1234@gmail.com>
2026-02-19 05:45:36 +0530

    Task 8 Completed

---
+++ 8.js
@@ -0,0 +1,8 @@
+let arr = ["Pappya", "Water Melon", "Apple", "Nancy", "Dancing"];
+
+function SortArr(arr) {
+  return arr.sort();
+}
+
+console.log("Original Array : ", arr);
+console.log("Sorted Array : ", SortArr(arr));
