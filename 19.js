5155f95238ec3f53c2c3f52f796a2b1059429116
Harshit Jain <harshitostwal1234@gmail.com>
2026-02-19 19:22:29 +0530

    Task 19 Completed

---
+++ 19.js
@@ -0,0 +1,25 @@
+const obj = {
+  name: "Harshit Jain",
+  age: 22,
+  city: "Chennai",
+  gender: "Male",
+  hobbies: ["Coding", "Traveling", "Cooking"],
+  greet: function () {
+    console.log("Hello, I am " + this.name + " from " + this.city);
+  },
+};
+
+function checkProperty(obj, key) {
+  if (obj.hasOwnProperty(key)) {
+    console.log("Key exists in the object.");
+  } else {
+    console.log("Key does not exist in the object.");
+  }
+  return obj;
+}
+
+console.log("Original Object : ", obj);
+const updatedObj = checkProperty(obj, "hobbies");
+console.log("Value : ", updatedObj);
+const updatedObj1 = checkProperty(obj, "bodyType");
+console.log("Value : ", updatedObj1);
